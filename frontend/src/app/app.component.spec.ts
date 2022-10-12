fe891d7a45f5140edcb91c80db58fc1dce696f21
Carlos Soria <dev11.sitenso@tensolite.com>
2022-10-12 10:48:44 -0300

    Test10

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -36,8 +36,10 @@ describe('AppComponent', () => {
     expect(suma).toBe(5);
   });
 
-  it('suma 3+3 = 6', () => {
-    const suma = 3+3;
-    expect(suma).toBe(6);
+  it('suma 1+1 = 2', () => {
+    const suma = 1+1;
+    expect(suma).toBe(2);
   });
+
+
 });
